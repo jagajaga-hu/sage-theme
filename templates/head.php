87c595f778818554d54d546065589f61c4441798
Leo Uno <reo@MacBook-Air.local>
2016-05-08 16:05:13 +0900

    add dependencies for jsTile

--- templates/head.php
+++ templates/head.php
@@ -4,4 +4,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
   <?php wp_head(); ?>
+  <!-- ここからjsTileスクリプト（js, css）と、jquery.easing（本当はこれらはmanifest.jsonに書きたい）-->
+  <script type="text/javascript"src="<?php echo get_template_directory_uri(); ?>/assets/scripts/jquery.easing.1.3.js"></script>
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/styles/tl-style.css" rel="stylesheet" />
+  <script src="<?php echo get_template_directory_uri(); ?>/assets/scripts/jstiles.js"></script>
+  <!-- ここまでjsTileスクリプト... -->
 </head>
