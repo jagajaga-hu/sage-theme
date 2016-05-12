b5298a4d6c48beeaef3beee1e7acb6f891055090
Leo Uno <leouno12@gmail.com>
2016-05-13 01:16:14 +0900

    modified

--- single.php
+++ single.php
@@ -1,3 +1,7 @@
+<div class="row">
+<div class="col-sm-8 col-sm-offset-2">
 ここからsingle.php<br>
 <?php get_template_part('templates/content-single', get_post_type()); ?>
 こここまでsingle.php
+</div>
+</div>
