98893d73badb1ec708a86056faf475f86f4a2aa6
Oke <oke@complex.ist.hokudai.ac.jp>
2016-05-14 17:35:34 +0900

    a

--- index.php
+++ index.php
@@ -30,9 +30,9 @@
     			    <a href="<?php the_permalink(); ?>">
                     <?php $postthumb = wp_get_attachment_image_src( get_post_thumbnail_id(), 'medium') ; ?>
                     <?php if($counter>4 || $counter==1): ?>
-                        <div class="jstile-img col-xs-12" style="background-image: url(<?php echo $postthumb[0]; ?>)">
+                        <div class="jstile-img col-xs-12" style="background: url("<?php echo $postthumb[0]; ?>")">
                     <?php else: ?>
-                        <div class="jstile-img col-xs-8" style="background-image: url(<?php echo $postthumb[0]; ?>)">
+                        <div class="jstile-img col-xs-8" style="background: url("<?php echo $postthumb[0]; ?>")">
                     <?php endif; ?>
                     </div>
                     <?php if($counter>4 || $counter==1): ?>
