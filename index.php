ba1ac45c127f0e17f79a137a6ae6c1f0ef743117
Oke <oke@complex.ist.hokudai.ac.jp>
2016-05-14 13:56:52 +0900

    a

--- index.php
+++ index.php
@@ -28,13 +28,13 @@
                 <div class="jstile row">
                     <article <?php post_class('top_page'); ?>>
     			    <a href="<?php the_permalink(); ?>">
-                    <div class="col-xs-8">
+                    <div class="jstile-img col-xs-8">
                     <?php if( has_post_thumbnail() ): ?>
                         <?php $postthumb = wp_get_attachment_image_src( get_post_thumbnail_id(), 'medium') ; ?>
                         <img src="<?php echo $postthumb[0]; ?>" alt="">
                     <?php endif; ?>
                     </div>
-                    <div class="col-xs-4">
+                    <div class="jstile-text col-xs-4">
     				<h2><?php the_title(); ?></h2>
                     <?php the_excerpt(); ?>»続きを読む
                     </div>
