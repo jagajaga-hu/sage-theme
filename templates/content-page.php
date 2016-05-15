37715c4c8c28621e8e23e440134541777e86719f
Leo Uno <leouno12@gmail.com>
2016-05-16 03:52:57 +0900

    modified content-page.php

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,4 +1,2 @@
-ここからtemplates/content-page.php
 <?php the_content(); ?>
 <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
-ここまでtemplates/content-page.php
