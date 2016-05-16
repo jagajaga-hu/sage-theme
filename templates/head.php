fdb0eeb4db262212946896db2a26f52b4288c5cb
Oke <oke@complex.ist.hokudai.ac.jp>
2016-05-17 02:38:38 +0900

    favicon設定をheadに記述

--- templates/head.php
+++ templates/head.php
@@ -9,9 +9,30 @@
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/tab.js"></script>
   <!-- jsTileのサンプルで使ってたフォントをとりあえず入れてみた -->
   <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300&subset=latin,greek-ext' rel='stylesheet' type='text/css'>
-    <link rel="icon" type="image/vnd.microsoft.icon" href="/favicon.ico">
-  <?php wp_head(); ?>
-  
+  <?php wp_head(); ?>   
+  <!-- faviconのセット -->  
+<link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/favicons/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/favicons/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/favicons/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/favicons/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/favicons/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/favicons/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/favicons/favicon-194x194.png" sizes="194x194">
+<link rel="icon" type="image/png" href="/favicons/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="/favicons/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="/favicons/manifest.json">
+<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#003e21">
+<meta name="apple-mobile-web-app-title" content="JagaJaga">
+<meta name="application-name" content="JagaJaga">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-TileImage" content="/favicons/mstile-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
   <!-- ここからjsTileスクリプト（js, css）と、jquery.easing（本当はこれらはmanifest.jsonに書きたい）-->
   <script type="text/javascript"src="<?php echo get_template_directory_uri(); ?>/assets/scripts/jquery.easing.1.3.js"></script>
   <script src="<?php echo get_template_directory_uri(); ?>/jsTileTemplate/jstiles.js"></script>
