3f785ddf25905049c13526303206558d33f7d94b
Oke <oke@complex.ist.hokudai.ac.jp>
2016-05-17 19:11:23 +0900

    get sudebar

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,13 +1,11 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
-    <header>
       <h2 class="entry-title"><?php the_title(); ?></h2>
       <?php get_template_part('templates/entry-meta'); ?>
-    </header>
     <div class="entry-content">
       <?php the_content(); ?>
     </div>
-    <footer>
+    <?php /** 
       <?php wp_link_pages(['before' => '<nav class="page-nav"><ul>' . __('', 'sage'),
                            'after' => '</ul></nav>',
                            'link_before' => '<li>',
@@ -19,6 +17,6 @@
                            'after' => '</ul></nav>',
                            'link_before' => '<li>',
                            'link_after' => '</li>']); ?>
-    </footer>
+                           **/ ?>
   </article>
 <?php endwhile; ?>
