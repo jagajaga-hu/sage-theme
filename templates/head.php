a805dd4be0586041921b517111c9478352b4ec1d
Jaga Jaga <jagajaga-hu@gmail.com>
2016-05-21 09:51:50 +0000

    a

--- templates/head.php
+++ templates/head.php
@@ -3,9 +3,6 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <script type="text/javascript" src="https://code.jquery.com/jquery-1.7.2.min.js"   integrity="sha256-R7aNzoy2gFrVs+pNJ6+SokH04ppcEqJ0yFLkNGoFALQ="   crossorigin="anonymous"></script>
-　　<script type="text/javascript">
-    var $172 = $.noConflict(true);
-　　</script>
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/tab.js"></script>
   <!-- jsTileのサンプルで使ってたフォントをとりあえず入れてみた -->
   <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300&subset=latin,greek-ext' rel='stylesheet' type='text/css'>
