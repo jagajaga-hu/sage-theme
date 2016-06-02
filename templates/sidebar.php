35f0c1ac68c9e3fb91748adee26f1e3ff96bf401
Jaga Jaga <jagajaga-hu@gmail.com>
2016-06-03 00:38:24 +0900

    fix ranking

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,16 +1,41 @@
 <?php /** GUI からのウィジェットは入れない! 
 dynamic_sidebar('sidebar-primary'); **/ ?>
 
+&nbsp;
+
 <div class="wpp-article">
-    <span class="weeklyranking">
+    <span class="ranking">
         Weekly Ranking
     </span>
 </div>
     
 <?php
 $wpp = array (
+    'range' => 'weekly', /*集計期間の設定（daily,weekly,monthly）*/
+    'limit' => 5, /*表示数はmax5記事*/
+    'post_type' => 'post', /*投稿のみ指定（固定ページを除外）*/
+    'title_length' => '35', /*タイトル文字数上限*/
+    'stats_comments' => '0', /*コメント数は非表示*/
+    'stats_views' => '1', /*閲覧数を表示させる*/
+    'thumbnail_width' => '150', /*画像のwidth（px）*/
+    'thumbnail_height' => '100', /*画像のheight（px）*/
+    'post_html' => '<div class="wpp-article"><div class="wpp-article-img">{thumb}</div><div class="wpp-article-text">{title}</div></div>', /*表示されるhtmlの設定（サムネイル、記事タイトル、の順で表示）*/
+); ?>
+
+<?php wpp_get_mostpopular($wpp); ?>
+
+&nbsp;
+
+<div class="wpp-article">
+    <span class="ranking">
+        Daily Ranking
+    </span>
+</div>
+
+<?php
+$wpp = array (
     'range' => 'daily', /*集計期間の設定（daily,weekly,monthly）*/
-    'limit' => 4, /*表示数はmax5記事*/
+    'limit' => 5, /*表示数はmax5記事*/
     'post_type' => 'post', /*投稿のみ指定（固定ページを除外）*/
     'title_length' => '35', /*タイトル文字数上限*/
     'stats_comments' => '0', /*コメント数は非表示*/
