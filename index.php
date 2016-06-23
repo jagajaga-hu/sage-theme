d7919e6f797854ea73e629d2834bd0e755a1c541
Jaga Jaga <jagajaga-hu@gmail.com>
2016-06-23 19:21:06 +0900

    a

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
 
-<?php echo do_shortcode("[huge_it_slider id='5']"); ?>
+<?php echo do_shortcode("[huge_it_slider id='2']"); ?>
 
 <nav class="nav-horizontal" id="global-nav">
 <div class="mask">
